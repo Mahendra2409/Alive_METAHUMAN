5be33486cd90e4116a2d51a2f8eb119a18f26816
Mahendra2409 <ms5268275@gmail.com>
2025-05-07 17:26:12 +0530

    commit

--- NeuroSync/NeuroSync_Player-main/llm_to_face.py
+++ NeuroSync/NeuroSync_Player-main/llm_to_face.py
@@ -1,6 +1,3 @@
-# This software is licensed under a **dual-license model**
-# For individuals and businesses earning **under $1M per year**, this software is licensed under the **MIT License**
-# Businesses or organizations with **annual revenue of $1,000,000 or more** must obtain permission to use this software commercially.
 
 # llm_to_face.py
 import pygame
